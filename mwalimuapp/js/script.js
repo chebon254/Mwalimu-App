9c803600d05e3d794ac7272a523add07750022d0
chebon254 <kelvinchebon90@gmail.com>
2023-06-10 21:59:20 +0300

    Code Changes

---
+++ mwalimuapp/js/script.js
@@ -0,0 +1,46 @@
+/* == BACK TO TOP == */
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+/* == BACK TO TOP == */
+//Create A Responsive Menu Hide Show Side Menu
+const header = document.querySelector(".header-scroll");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const menu = document.querySelector(".links");
+// Onscroll Change Nav Bar Background
+window.onscroll = ()=>{
+    this.scrollY > 20 ? header.classList.add("sticky") : header.classList.remove("sticky");
+}
+/* == ANIMATION CSS == */
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 1;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("showreveal");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+/* == ANIMATION CSS == */
+
+// Onclick For Menu Bar
+menuBtn.onclick = ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+}
+
+// Onclick For Cancel Menu 
+cancelBtn.onclick = ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+}
+
